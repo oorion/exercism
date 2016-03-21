a33b0ecde646971da23ecc9767b25ebaad2fc3db
Orion Osborn <oosborn@invoca.com>
2016-03-20 20:23:55 -0700

    complete prime-factors in python

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,19 @@
+def prime_factors(number):
+    prime_factor_list = []
+    while True:
+        for n in range(2, number + 1):
+            if is_prime(n) and number % n == 0:
+                pf_list.append(n)
+                number = number / n
+                break
+        if number == 1:
+            break
+    return prime_factor_list
+
+def is_prime(number):
+    if number == 1:
+        return False
+    for n in range(2, number):
+        if number % n == 0:
+            return False
+    return True
