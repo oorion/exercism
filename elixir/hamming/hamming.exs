75045f454e0a2743058d2d8524ff2fd93af491de
Orion Osborn <oosborn@invoca.com>
2016-06-03 14:40:52 -0600

    update hamming in elixir

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,21 +8,20 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
-    if length(strand1) != length(strand2) do
-      nil
-    else
-      strand1_with_index = Enum.with_index(strand1)
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2) do
+  end
 
-      fun = fn(x, acc) ->
-        if elem(x, 0) != Enum.at(strand2, elem(x,1)) do
-          acc = acc + 1
-          acc
-        else
-          acc
-        end
+  def hamming_distance(strand1, strand2) do
+    strand1_with_index = Enum.with_index(strand1)
+
+    Enum.reduce(strand1_with_index, 0, fn(x, acc) ->
+      if elem(x, 0) != Enum.at(strand2, elem(x,1)) do
+        acc = acc + 1
+        acc
+      else
+        acc
       end
-      Enum.reduce(strand1_with_index, 0, fun)
     end
+    )
   end
 end
