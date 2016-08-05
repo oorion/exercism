4cbd8b0f1c529a95849f826680dd1162e6810a32
Orion Osborn <oosborn@invoca.com>
2016-08-05 15:53:41 -0700

    2nd version of nth prime in elixir

--- elixir/nth-prime/nth_prime.exs
+++ elixir/nth-prime/nth_prime.exs
@@ -5,8 +5,33 @@ defmodule Prime do
   """
   @spec nth(non_neg_integer) :: non_neg_integer
   def nth(count) do
-    primes = Enum.filter(2..1000, &is_prime(&1)) |> List.to_tuple
-    elem(primes, count - 1)
+    if count == 0 do
+      raise Error
+    end
+    prime_number_and_count = {2, 1}
+    find_nth_prime(prime_number_and_count, count)
+  end
+
+  def find_nth_prime(prime_number_and_count, desired_count) do
+    if elem(prime_number_and_count, 1) == desired_count do
+      elem(prime_number_and_count, 0)
+    else
+      current_prime = elem(prime_number_and_count, 0)
+      current_count = elem(prime_number_and_count, 1)
+      {find_next_prime(current_prime), current_count + 1} |> find_nth_prime(desired_count)
+    end
+  end
+
+  def find_next_prime(prime_number) do
+    find_next_prime(prime_number, 1)
+  end
+
+  def find_next_prime(prime_number, n) do
+    if is_prime(prime_number + n) do
+      prime_number + n
+    else
+      find_next_prime(prime_number, n + 1)
+    end
   end
 
   def is_prime(n) do
